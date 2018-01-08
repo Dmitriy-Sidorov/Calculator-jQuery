90f0feeb19875beb4bbb9507e68441ddc4d1f5dd
Dmitriy Sidorov <DmitriyWhite@icloud.com>
2018-01-08 19:53:52 +0300

    design of the calculator

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,24 @@
-//custom
+$(document).ready(function () {
+    $('.calc select').change(function () {
+        $edition = $('select#card_edition').val();
+        $paper = $('select#card_paper').val();
+        $color = $('select#card_color').val();
+        $print_file = $('select#card_color option:selected').attr('data-print-file');
+        $paper_ratio = $('select#card_paper option:selected').attr('data-paper-ratio');
+        $color_format = $('select#card_color option:selected').attr('data-color');
+        $print = $('select#card_color option:selected').attr('data-print');
+
+        $division = 30;
+        $final_edition = $edition / $division;
+        $price = $final_edition * $paper_ratio + parseInt($print_file) + $final_edition*$print;
+
+        console.log($price);
+
+        $('span#final_price').text(Math.round($price));
+        if ($color_format) {
+            $('.card-images img').hide();
+            $('#card_' + $color_format).show();
+        }
+    });
+});
+
